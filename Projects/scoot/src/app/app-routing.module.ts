697cbd92ede7b75cded9d6a081ac8746cd316c0b
Timothy Henderson <titansmith8@hotmail.com>
2022-09-23 12:08:37 -0400

    six base comps, routing half set

--- Projects/scoot/src/app/app-routing.module.ts
+++ Projects/scoot/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CareersComponent } from './components/careers/careers.component';
+import { HomeComponent } from './components/home/home.component';
+import { LocationsComponent } from './components/locations/locations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/home", pathMatch: "full"},
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {path: "careers", component: CareersComponent}, 
+  {path: "locations", component: LocationsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Projects/scoot/src/app/app.module.ts
+++ Projects/scoot/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { CareersComponent } from './components/careers/careers.component';
+import { LocationsComponent } from './components/locations/locations.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    CareersComponent,
+    LocationsComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
