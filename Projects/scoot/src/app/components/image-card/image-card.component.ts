0eac8b03307e50d44abc21048095e0eb67f64af7
Timothy Henderson <titansmith8@hotmail.com>
2022-09-25 20:07:26 -0400

    I got a card component done

--- Projects/scoot/src/app/components/image-card/image-card.component.ts
+++ Projects/scoot/src/app/components/image-card/image-card.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-image-card',
   templateUrl: './image-card.component.html',
-  styleUrls: ['./image-card.component.css']
+  styleUrls: ['./image-card.component.css'],
+  providers: [DataService]
 })
 export class ImageCardComponent implements OnInit {
+  imageCard!: { image: string; numbImage: string; caption: string; content: string; }[];
 
-  constructor() { }
+  constructor(private data: DataService) { }
 
   ngOnInit(): void {
+    this.imageCard = this.data.imageCard
+    console.log(this.imageCard)
   }
-
+  
+  
 }
--- Projects/scoot/src/app/services/data.service.ts
+++ Projects/scoot/src/app/services/data.service.ts
@@ -9,20 +9,20 @@ export class DataService {
 
   imageCard = [
     {
-      image: "../../../assets/images/our-tech.jpg",
-      numbImage: "https://cdn.discordapp.com/attachments/988892969782354062/1023715185145421894/unknown.png",
+      image: '../../../assets/images/our-tech.jpg',
+      numbImage: 'https://cdn.discordapp.com/attachments/988892969782354062/1023715491816157224/unknown.png',
       caption: "Our tech",
       content: "We’re using cutting edge technology to drive accessible urban transportation forward. Our fully electric scooters are a joy to ride!",
     },
     {
-      image: "../../../assets/images/our-integrity.jpg",
-      numbImage: "https://cdn.discordapp.com/attachments/988892969782354062/1023715385381498910/unknown.png",
+      image: '../../../assets/images/our-integrity.jpg',
+      numbImage: 'https://cdn.discordapp.com/attachments/988892969782354062/1023715385381498910/unknown.png',
       caption: "Our integrity",
       content: "We are fully committed to deliver a great yet safe, sustainable micro-mobility experience in every city we serve.",
     },
     {
-      image: "../../../assets/images/our-community.jpg",
-      numbImage: "https://cdn.discordapp.com/attachments/988892969782354062/1023715185145421894/unknown.png",
+      image: '../../../assets/images/our-community.jpg',
+      numbImage: 'https://cdn.discordapp.com/attachments/988892969782354062/1023715185145421894/unknown.png',
       caption: "Our community",
       content: "We support every community we serve. All workers are paid a living wage based on their location and are Scoot employees.",
     }
